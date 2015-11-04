4d4d46852ee01f4ee8deae51b6c93fc4903e3cf6
Silvia Ehrmann <Silvia Ehrmann>
2015-11-04 23:02:33 +0100

    VimCrypt~01!@è¬욦ä/%$ÌóçMÁ·ӕm,qÖÚyèÝþÏq«NáGñg­ofBaÏNÃn±Êb% |ܼ>ÜKè¿ÑÕÐQêÎOO®áՎ!^ìÿÆ-âî2û¢ùÄÛ?$ºI.èÐ]øDcþÉiK<\þ¡3ò6@	$¶ð¸¤'嗐=u®Ãì&^@³wéÒzÿE<è¬wÙ{)3ú5gŜ9a·¨å'=fD$}µ»Pº8­KÌâ1_Õ%eöNПàòP&ùÎ+%KÝìſ§V¦_¡4g؉Á¡ö©6ïýRß/è;¨¾`?ò[VXì7¤uԫ¥4(bS߄8¾{ד@WӘiFÄ\âùOíQ;xp7ÎµQñ½k£tت-¿0:lúé«ï©dä#Fy9WÈF­°ËOÅMætpj޿G7yç¿×zìUCµq3ÈQ²`öhߓÝ£Yæòë,_J¹ÎnýTWd|[)çÆx¾d¡ܻH3<e¬u+»OBDÿ]Upá

--- logmap.cxx
+++ logmap.cxx
@@ -1,23 +1,44 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
 
 using namespace std;
 
-int main(){
-	double x0 = 0.5;
+int main()
+{
+	double x0 = 0.5;  //Startwert
 	double x;
-	int Nskip = 100; //Number of iterations to skip
-	int Nend  = 200; //Number of total iterations
+	int Nskip = 0;  //Iterationsanzahl ohne Speicherung
+	int Nend  = 100;  //Iterationsanzahl insgesamt
+	int size=Nend-Nskip;  //Arraylänge für Datenspeicherung
+	double xn[size];  //Array zurZwischenspeicherung
+	
+	fstream out("Daten.txt",ios::out);	
+	fstream outplot("Daten_plot.txt",ios::out);
 
-	for(double r=0; r <= 4; r += 0.001){
+	for(double r=0; r <= 4; r += 0.001)
+	{
 	   x=x0;
 	   for(int i=1; i <= Nskip; i++)
 		   x = r*x*(1-x);
-	   for(int i=Nskip+1; i <= Nend; i++){
+	   for(int i=0; i+Nskip < Nend; i++)
+	   {
 	   		   x = r*x*(1-x);
-	   		   cout << r << "\t" << x << endl;
+	   		   xn[i]=x;   
    	   }
+	   for(int i=0;i<size;i++)
+	   {
+	   		out<<r<<"\t"<<xn[i]<<endl;
+	   }	   
 	}
-
-
+	out.close();	
+	
+	//Plot in Gnuplot erstellen und speichern
+	outplot<<"set terminal jpeg"<<endl;
+	outplot<<"set output \"Plot.jpeg\" "<<endl;
+	outplot<<"plot \"Daten.txt\" u 1:2 w dots"<<endl;
+	outplot.close();
+	
+	system("gnuplot  \"Daten_plot.txt\" ");
 	return 0;
 }
